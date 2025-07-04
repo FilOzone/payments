85a0740ef1556f77b789702eb41da538f8227056
Aashish Paliwal <aashish.paliwal@live.com>
2025-07-04 20:02:12 +0530

    chore: bump up solidity version to atleast 0.8.27

--- src/Errors.sol
+++ src/Errors.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.27;
 
 /// @title Errors
 /// @notice Centralized library for custom error definitions across the protocol
